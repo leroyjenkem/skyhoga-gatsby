bd60ced4ccf531f996a1066ce2b7e5bbe03085c7
leroyjenkem <96355964+leroyjenkem@users.noreply.github.com>
2022-01-03 18:54:09 +0200

    adw

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,3 +1,6 @@
+import * as React from 'react',
+import { graphql } from "gatsby"
+
 export const projectsPageQuery = graphql`
   query projectsPageQuery {
     wordpressPage(slug: { eq: "portfolio" }) {
