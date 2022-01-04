5e24dffe1788d24f4d81adad9b76ab1b41c0a107
leroyjenkem <96355964+leroyjenkem@users.noreply.github.com>
2022-01-04 22:02:24 +0200

    awdad

---
+++ src/components/PostImage.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import parse from 'html-react-parser'
+
+
+
+
+export const PostImage = ({src, alt, width}) => {};
--- src/pages/GalleryPage.js
+++ src/pages/GalleryPage.js
@@ -1,70 +1,15 @@
 import React from "react"
 import { graphql } from "gatsby"
+import parse from 'html-react-parser';
+import PostImage from '../components/PostImage'
 
-const md = (data) => {
-  const imagesResolutions = data.allWordpressPost.edges.map(
-  edge =>
-    edge.node.childWordPressAcfPostPhoto.photo.localFile.childImageSharp.fixed)
 
-  const { frontmatter, html } = data.data.allMarkdownRemark.edges[0].node
-  console.log(frontmatter.title)
-  return (<div>
-    <h1>{frontmatter.title}</h1>
-    <p>{frontmatter.date}</p>
-    {imagesResolutions.map(imageRes => (
-       <Img fixed={imageRes} key={imageRes.src} />
-     ))}
-    <div
-      dangerouslySetInnerHTML={{ __html: html }}
-    />
-  </div>)
-}
 
 const GalleryPage = ({ data }) => (
   <div>
-)
-    {md({ data })}
-  </div>
+     </div>
 )
 
-export const query = graphql`
-query {
-  allMarkdownRemark(
-    filter: {fileAbsolutePath: {regex: "/gallery/"}}
-  ) {
-      edges {
-        node {
-          frontmatter {
-            title
-            date(formatString: "DD MMMM YYYY")
-          }
-          html
-        }
-      }
-    }
-}
-`
-export const imgquery = imggraphql`
-  imgquery {
-    allWordpressPost {
-      edges {
-        node {
-          childWordPressAcfPostPhoto {
-            photo {
-              localFile {
-                childImageSharp {
-                  # edit the maxWidth value to generate resized images
-                  fixed(width: 500, height: 500) {
-                    ...GatsbyImageSharpFixed_withWebp_tracedSVG
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
+
 
 export default GalleryPage;
